24ecfd55c2970060c102a9b807e6bcf91816dab6
eli <frozenpandaman@gmail.com>
2015-10-01 10:51:38 -0700

    init. started unity project!

---
+++ Assets/Pause.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	bool paused = false;
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			gamePaused();
+		}
+	}
+
+	void OnGUI()
+	{
+		if (paused == true)
+		{
+			GUILayout.Label("Paused.");
+			if(GUILayout.Button ("Click to unpause."))
+			{
+			gamePaused();
+			}
+		}
+	}
+
+	void gamePaused()
+	{
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			paused = true;
+		}
+
+		else
+		{
+			Time.timeScale = 1;
+			paused = false;
+		}
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody rb;
+	Vector3 velocity;
+
+	// Use this for initialization
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
+
+	void FixedUpdate() { // rb.position = starting pos.
+		rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	}
+}
