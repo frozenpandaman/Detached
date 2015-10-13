c24da8820d440813e65969c82aa584b03382e57e
eli <frozenpandaman@gmail.com>
2015-10-12 20:53:17 -0700

    soul now gets farther away every trigger enter!

--- Assets/ResetPos.cs
+++ Assets/ResetPos.cs
@@ -3,8 +3,25 @@ using System.Collections;
 
 public class ResetPos : MonoBehaviour {
 
+	public GameObject player;
+	public Transform soul;
+
+	private float currentOffset = 0f;
+
+	void Start() {
+		UpdateOffset();
+	}
+
+	void UpdateOffset() {
+		currentOffset = soul.transform.position.x;
+	}
+
 	void OnTriggerEnter (Collider other) {
-		other.transform.position = new Vector3(0f,0.5f,-20f);
+		if (other.gameObject == player) {
+			other.transform.position = new Vector3(0f,0.5f,-20f);
+			UpdateOffset();
+		}
+		soul.transform.position = new Vector3(currentOffset+5f,0.5f,-20f);
 		// Make soul/camera farther apart
 		// Re-generate terrain (PCG)
 	}
