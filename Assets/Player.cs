6eb9720b3699f8a384f12e61feb7643cced38ccc
eli <frozenpandaman@gmail.com>
2015-10-12 20:17:07 -0700

    got triggers working!

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,40 +1,22 @@
-﻿ using UnityEngine;
- using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 
- public class playerController : MonoBehaviour {
+public class Player : MonoBehaviour {
 
-     public float sensitivityX = 15F;
-     public float runSpeed = 10,
-               sprintSpeed = 20;
-     private Transform cameraTransform;
+	Rigidbody rb;
+	Vector3 velocity;
 
+	// Use this for initialization
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
 
-     // Use this for initialization
-     void Start () {
-         cameraTransform = Camera.main.transform;
-     }
+	// Update is called once per frame
+	void Update() {
+		velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
 
-     // Update is called once per frame
-     void Update () {
-         if (Input.GetKey("w")) {
-         	print("hi");
-            if (Input.GetKey (KeyCode.LeftShift)) {
-              transform.position += cameraTransform .forward * (Time.deltaTime * sprintSpeed);
-              Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 80, 2*Time.deltaTime);
-            }
-            else {
-              transform.position += cameraTransform .forward * (Time.deltaTime * runSpeed);
-              Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60, 2*Time.deltaTime);
-            }
-         }
-         if (Input.GetKey("s")) {
-            transform.position += Vector3.back * (Time.deltaTime * runSpeed);
-         }
-         if (Input.GetKey("a")) {
-            transform.position += Vector3.left * (Time.deltaTime * runSpeed);
-         }
-         if (Input.GetKey("d")) {
-            transform.position += Vector3.right * (Time.deltaTime * runSpeed);
-         }
-     }
- }
+	void FixedUpdate() { // rb.position = starting pos.
+		rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/ResetPos.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPos : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		other.transform.position = new Vector3(0f,0.5f,-20f);
+		// Make soul/camera farther apart
+		// Re-generate terrain (PCG)
+	}
+
+	//void OnTriggerStay (Collider other) {
+	//}
+	//void OnTriggerExit (Collider other) {
+	//}
+}
