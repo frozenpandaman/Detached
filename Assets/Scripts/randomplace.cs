a1fb6112b15ef312a0d37160d4f76485e58d544d
tyang000 <tyang@g.hmc.edu>
2015-11-12 00:55:00 -0800

    with four shperes randomly placed exammples added

---
+++ Assets/Scripts/randomplace.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomplace : MonoBehaviour {
+    public GameObject[] stuffToPlace;
+    private int[,] grid = new int[6, 6];
+    Vector3 targetCoords;
+    public int maxNumberOfObjectsToPlace = 30;
+    // Use this for initialization
+    void Start () {
+        float scale = 1f;
+        float moveAreaX = gameObject.GetComponent<Renderer>().bounds.size.x / 2;
+        float moveAreaZ = gameObject.GetComponent<Renderer>().bounds.size.z / 2;
+        Vector3 center = gameObject.GetComponent<Renderer>().bounds.center;
+        foreach (GameObject placeable in stuffToPlace)
+        {
+            float targetCoordsX = center.x + Random.Range(-moveAreaX * scale, moveAreaX * scale);
+            float targetCoordsZ = center.z + Random.Range(-moveAreaZ * scale, moveAreaZ * scale);
+            targetCoords = new Vector3(targetCoordsX, 2.1f, targetCoordsZ);
+            placeable.transform.position = targetCoords;
+            print(placeable);
+        }
+    }
+
+        // Update is called once per frame
+        void Update () {
+	
+	}
+}
