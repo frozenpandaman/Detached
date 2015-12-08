88cb57188eda296afeff74d01b4cc662b25cd369
tyang000 <tyang@g.hmc.edu>
2015-12-07 23:57:36 -0800

    enlarged map and added randomwalls

--- Assets/Scripts/randomplace.cs
+++ Assets/Scripts/randomplace.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class randomplace : MonoBehaviour {
     public GameObject[] stuffToPlace;
+    public GameObject wall;
     public GameObject grass;
+    public GameObject stone;
     private int[,] grid = new int[6, 6];
     Vector3 targetCoords;
     public int maxNumberOfObjectsToPlace = 30;
@@ -15,14 +17,25 @@ public class randomplace : MonoBehaviour {
 
         Vector3 center = gameObject.GetComponent<Renderer>().bounds.center;
         int numofgrass = 100;
+        int numofstone = 50;
+        int numofwall = 15;
         while (numofgrass > 0)
         {
             float targetCoordsX = center.x + Random.Range(-moveAreaX * scale, moveAreaX * scale);
             float targetCoordsZ = center.z + Random.Range(-moveAreaZ * scale, moveAreaZ * scale);
-            targetCoords = new Vector3(targetCoordsX, 0f, targetCoordsZ);
+            targetCoords = new Vector3(targetCoordsX, -1.0f, targetCoordsZ);
             Instantiate(grass, targetCoords, Quaternion.Euler(0, Random.Range(0f, 359f), 0));
             --numofgrass;
         }
+        while (numofstone > 0)
+        {
+            float targetCoordsX = center.x + Random.Range(-moveAreaX * scale, moveAreaX * scale);
+            float targetCoordsZ = center.z + Random.Range(-moveAreaZ * scale, moveAreaZ * scale);
+            targetCoords = new Vector3(targetCoordsX, 0.0f, targetCoordsZ);
+            Instantiate(stone, targetCoords, Quaternion.Euler(0, Random.Range(0f, 359f), 0));
+            --numofstone;
+        }
+
         foreach (GameObject placeable in stuffToPlace)
         {
             float targetCoordsX = center.x + Random.Range(-moveAreaX * scale, moveAreaX * scale);
@@ -30,6 +43,26 @@ public class randomplace : MonoBehaviour {
             targetCoords = new Vector3(targetCoordsX,0.2f, targetCoordsZ);
             placeable.transform.position = targetCoords;
             print(placeable);
+        }
+
+        while (numofwall > 0)
+        {
+            float targetCoordsX = center.x + Random.Range(-moveAreaX * scale, moveAreaX * scale);
+            float targetCoordsZ = center.z + Random.Range(-moveAreaZ * scale, moveAreaZ * scale);
+            targetCoords = new Vector3(targetCoordsX, 1.0f, targetCoordsZ);
+            Object clone;
+            float changedlength = Random.Range(30f, 70f);
+            wall.transform.localScale += new Vector3(-0.5f, 0, changedlength);
+            if (Random.Range(0f,359f)>180f)
+            {
+                clone = Instantiate(wall, targetCoords, Quaternion.Euler(0, 90, 0));
+            }
+            else
+            {
+                clone = Instantiate(wall, targetCoords, Quaternion.Euler(0, 0, 0));
+            }
+            wall.transform.localScale += new Vector3(0.5f, 0, -changedlength);
+            --numofwall;
         }
     }
 
